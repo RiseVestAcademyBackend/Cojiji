08ee596af4f8921cc0e624bca9cef689d75114f9
Oluwafemi <ojurifemi132@gmal.com>
2025-03-12 13:20:05 +0100

    reformatted and removed unused import.

--- models/user.js
+++ models/user.js
@@ -1,54 +1,55 @@
-const { Sequelize, DataTypes, Model } = require('sequelize');
-const sequelize = require('../config/db');
-const bcrypt = require('bcrypt');
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../config/db");
+const bcrypt = require("bcrypt");
 
 class User extends Model {}
 
-User.init({
+User.init(
+  {
     id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     username: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    phoneNumber : {
-        type: DataTypes.STRING,
-        allowNull: false
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     role: {
-        type: DataTypes.ENUM,
-        values: ['Seller', 'Buyer', 'Admin', 'Viewer'],
-        defaultValue: 'Viewer'
+      type: DataTypes.ENUM,
+      values: ["Seller", "Buyer", "Admin", "Viewer"],
+      defaultValue: "Viewer",
     },
-    
-}, {
+  },
+  {
     timestamps: true,
     sequelize,
-    modelName: 'User',
+    modelName: "User",
     hooks: {
-        beforeCreate: async (user) => {
-            if (user.password) {
-                const salt = await bcrypt.genSalt(10);
-                user.password = await bcrypt.hash(user.password, salt);
-            }
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
         }
-    }
-});
-
+      },
+    },
+  }
+);
 
 module.exports = User;
