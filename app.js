c84fb18920d6c37384310f81b02cfcb96ab89af9
ugochukwu-850 <ugochukwchizaramoku@gmail.com>
2025-03-13 14:54:23 +0100

    removed emojis

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var buyersRouter = require('./routes/buyers');
 
-var sequelize = require("./config/database")
+var sequelize = require("./config/database");
 
 var app = express();
 
@@ -46,11 +46,14 @@ app.use(function (err, req, res, next) {
 (async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ alter: true });
+    sequelize
+      .sync({ alter: true }) // Use { force: true } if you want to drop & recreate tables
+      .then(() => console.log("> Database & tables created!"))
+      .catch((err) => console.error("> Error syncing database:", err));
 
-    console.log("✅ Database connected successfully!");
+    console.log("> Database connected successfully!");
   } catch (error) {
-    console.error("❌ Database connection error:", error);
+    console.error("> Database connection error:", error);
   }
 })();
 
