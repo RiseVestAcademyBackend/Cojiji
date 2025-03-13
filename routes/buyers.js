a4c71fc99217c7b0cd1454c68c96e3cfbdadebe4
Tiffany Nebo <nebotiffany@gmail.com>
2025-03-13 15:31:21 +0100

    wrote controller code for verifying account, saving favs and viewing order history. the related routes were created too

--- routes/buyers.js
+++ routes/buyers.js
@@ -1,2 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const buyerController = require("../controllers/buyerController");
+ 
+router.get("/:buyerId/orders", buyerController.viewOrderHistory);
+router.patch("/:buyerId/verify", buyerController.verifyAccount);
+router.post("/favorites", buyerController.saveFavourite);
+ 
+
+module.exports = router;
