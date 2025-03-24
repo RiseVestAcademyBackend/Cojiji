8278e890a54e69ebf57e7cc62ac9340138af390e
Obiora Ebuka <94439139+oEbuka@users.noreply.github.com>
2025-03-24 12:08:27 +0100

    Update adminRoutes.js
    updated the necessary routes since users and posts don't exist.

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,19 +2,13 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
 
-// delete a post
-router.delete("/posts/:postId", adminController.deleteAd);
-
-// approve/reject posts
-router.put("/posts/:postId/status", adminController.approveOrRejectAd);
-
 // manage ads
 router.post("/ads", adminController.createAd);
 router.put("/ads/:adId", adminController.updateAd);
 router.delete("/ads/:adId", adminController.deleteAd);
 
 // ban users or flag content
-router.put("/users/:userId/ban", adminController.banUser);
+router.put("/buyers/:buyerId/ban", adminController.banUser);
 
 // view reports
 router.get("/reports", adminController.viewReports);
